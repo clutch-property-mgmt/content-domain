57494fc8dced879f98a58f86c025816290ce6e68
Eric Fortmeyer <e.fortmeyer01@gmail.com>
2026-03-17 18:44:24 -0600

    fix(Review): remove superfluous attribute, add labels to author and body properties
    Signed-off-by: Eric Fortmeyer <e.fortmeyer01@gmail.com>

--- src/Review.php
+++ src/Review.php
@@ -4,9 +4,11 @@ namespace ClutchPropertyMgmt\ContentDomain;
 
 use Phpolar\Model\AbstractModel;
 use Phpolar\Model\Hidden;
+use Phpolar\Model\Label;
 use Phpolar\Model\PrimaryKey;
 use Phpolar\Validators\Max;
 use Phpolar\Validators\MaxLength;
+use Phpolar\Validators\Min;
 use Phpolar\Validators\Required;
 
 final class Review extends AbstractModel
@@ -16,13 +18,16 @@ final class Review extends AbstractModel
     public int $id;
 
     #[Required]
+    #[Label("Your name")]
     public string $author;
 
     #[Max(10)]
-    #[Max(1)]
+    #[Min(1)]
+    #[Required]
     public int $rating;
 
     #[Required]
     #[MaxLength(1200)]
+    #[Label("Comments")]
     public string $body;
 }
